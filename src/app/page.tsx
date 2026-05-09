a9ae306ed5c991e27072ee20c9c0b810704bbbb4
Abdulwahab kayode <dahbiigkayode00@gmail.com>
2026-05-09 17:13:22 +0100

    project set up

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Cards from '@/components/Components/Cards'
+import React from 'react'
+
+
+const page = () => {
+  return (
+    <div className='h-[100vh] bg-red-400 w-[100vw]'>
+      <Cards/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/Components/Cards.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SpotlightCard from '@/components/SpotlightCard';
+  
+
+const Cards = () => {
+  return (
+   <SpotlightCard className="custom-spotlight-card  w-50 h-50" spotlightColor="rgba(0, 229, 255, 0.2) ">
+  // Content goes here
+</SpotlightCard> 
+  )
+}
+
+export default Cards
