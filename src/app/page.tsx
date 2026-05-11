e911caa800e3f2dbd0b40cb558ea36aa411b44a6
Abdulwahab kayode <dahbiigkayode00@gmail.com>
2026-05-11 18:57:37 +0100

    added card flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
-import Cards from '@/components/Components/Cards'
-import React from 'react'
-
+import Cards from "@/components/Components/Cards";
+import React from "react";
 
 const page = () => {
   return (
-    <div className='h-[100vh] bg-red-400 w-[100vw]'>
-      <Cards/>
-    </div>
-  )
-}
+    <>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+    <div className="flex items-center justify-center absolute top-20 bottom-20 left-20 right-20  grid-col-2  grid-rows-2 gap-4 ">
+     <div className="">
+      <div className="p-10">
+        <Cards title={"Home"} /> 
+      </div> <div className="p-10">
+        <Cards title={""} />
+         </div>
+      </div>      <div className="">
+      <div className="p-10">
+        <Cards title={""} /> 
+      </div> <div className="p-10">
+        <Cards title={""} />
+         </div>
+      </div></div>
+      
+    </>
+  );
+};
 
-export default page
+export default page;
--- src/components/Components/Cards.tsx
+++ src/components/Components/Cards.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import SpotlightCard from '@/components/SpotlightCard';
-  
+"use client"
+import React, { useState } from 'react'
 
-const Cards = () => {
+type CardProps = {
+  title: string;
+};
+
+function Cards({ title }: CardProps) {
+ const[expand,SetExpand] =useState(true);
+
+  const handleClick = () => {
+    SetExpand(!expand);
+  }
   return (
-   <SpotlightCard className="custom-spotlight-card  w-50 h-50" spotlightColor="rgba(0, 229, 255, 0.2) ">
-  // Content goes here
-</SpotlightCard> 
+    <div>
+   <div onClick={handleClick} className={` ${expand ? 'w-[100vw] z-50 h-[100vh]' : 'w-[100vw] z-50 h-[100vh]'} duration-400 overflow-hidden h-30 w-30 md:h-55 md:w-55 rounded-lg bg-neutral-800/50 backdrop-blur-sm border border-neutral-700/50 shadow-lg shadow-neutral-900/20`}> 
+      {title}
+       </div>
+    </div>
   )
 }
 
